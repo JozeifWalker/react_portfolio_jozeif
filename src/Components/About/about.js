d9ee26a74d57ce7d7ef1751a551d15b42eb7f080
Jozeif Walker <35747949+JozeifWalker@users.noreply.github.com>
2020-06-04 13:04:35 -0500

    Removed image in about page

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -7,30 +7,17 @@ import jozeif from "../../Images/jozeif.jpg";
 
 class About extends Component {
     render() {
-        const image = {
-            height: "45%",
-            width: "100%",
-            borderRadius: "50%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            padding: "10px"
-
-        }
         return (
             <div className="aboutme">
                 <body>
-                    <section className="aboutme-wrapper">
-
-                        <img className="jozeif" src={jozeif} />
-
-
+                    <div className="aboutme-wrapper">
+                        <img src={jozeif} className="jozeif" />
                         <h2>Jozeif Walker</h2>
                         <h3>Mobile/Web/Front-End Developer</h3>
                         <h4>Application Developer at IBM</h4>
-                        <p className="description">Front-End, Back-End, UI/UX, Mobile, Web or whatever you need. I am a developer that values creating positive and memorable experiences for your users.
+                        <p className="description">Front-End ,Back-End, Mobile, Web or whatever your needs are. I am a developer that values creating positive and memorable experiences for your users.
                         I am eager to learn in order to deliver optimal solutions to your clients. </p>
-                    </section>
+                    </div>
 
 
 
